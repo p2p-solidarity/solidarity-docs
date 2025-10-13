2d1a5847df399eb3f9901aac1acd8c7da649bf25
kidneyweakx <kidneyweakx@gmail.com>
2025-10-13 14:21:56 +0900

    docs(usage): describe app features

--- app/docs/usage/_meta.js
+++ app/docs/usage/_meta.js
@@ -1,6 +1,7 @@
 export default {
   index: 'Overview',
   'quick-start': 'Quick Start',
+  'features': 'App Features',
   'use-cases': 'Use Cases',
   'sharing-methods': 'Sharing Methods',
   'privacy-security': 'Privacy & Security'
