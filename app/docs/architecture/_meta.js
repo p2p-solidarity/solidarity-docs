42f76ce88d0ee48386c7128729d0a75e4379b93d
kidneyweakx <kidneyweakx@gmail.com>
2025-10-13 13:52:01 +0900

    docs(arch): update pass signing docs

--- app/docs/architecture/_meta.js
+++ app/docs/architecture/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: 'Overview',
   'privacy': 'Privacy & Security',
   'p2p-networking': 'P2P Networking',
+  'pass-signing': 'Pass Signing',
   'zero-knowledge': 'Zero-Knowledge Proofs',
   'tech-stack': 'Technology Stack'
 }
